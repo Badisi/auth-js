3e7dd182a24f7f67d38b5bb6b78fd4f6736744cc
Badisi <badisi84@gmail.com>
2022-02-09 20:46:58 +0100

    feat(demo-app): pretty print debug info

--- projects/demo-app/src/app/demo/debug/debug.module.ts
+++ projects/demo-app/src/app/demo/debug/debug.module.ts
@@ -1,17 +1,22 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { DebugComponent } from './debug.component';
+import { PrettyPrintPipe } from './pretty-print.pipe';
 
 @NgModule({
     declarations: [
-        DebugComponent
+        DebugComponent,
+        PrettyPrintPipe
     ],
     imports: [
         CommonModule
     ],
     exports: [
         DebugComponent
+    ],
+    providers: [
+        DatePipe
     ]
 })
 export class DebugModule { }
---
+++ projects/demo-app/src/app/demo/debug/pretty-print.pipe.ts
@@ -0,0 +1,39 @@
+import { DatePipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+
+const DATE_FORMAT = 'EEE, MMM dd y, hh:mm:ss a, zzzz';
+
+@Pipe({
+    name: 'prettyPrint'
+})
+export class PrettyPrintPipe implements PipeTransform {
+    constructor(
+        private datePipe: DatePipe
+    ) { }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public transform(value: any, dateAttrs: string[] = []): string {
+        const jsonLine = /^( *)("[\w-]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
+        const replacer = (_match: string, pIndent: string, pKey: string, pVal: string, pEnd: string): string => {
+            let r = pIndent || '';
+            const key = (pKey) ? pKey.replace(/[": ]/g, '') : undefined;
+            if (key) {
+                r += `<span class="json-key">${key}</span>: `;
+            }
+            if (pVal) {
+                r += `<span class="${pVal.startsWith('"') ? 'json-string' : 'json-value'}">${pVal}</span>`;
+                if (key && dateAttrs.includes(key)) {
+                    const pValAsDate = this.datePipe.transform(Number(pVal) * 1000, DATE_FORMAT)?.toString();
+                    if (pValAsDate) {
+                        r += ` <span class="json-date">(${pValAsDate})</span>`;
+                    }
+                }
+            }
+            return r + (pEnd || '');
+        };
+        return JSON.stringify(value, null, 2)
+            .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
+            .replace(/</g, '&lt;').replace(/>/g, '&gt;')
+            .replace(jsonLine, replacer);
+    }
+}
