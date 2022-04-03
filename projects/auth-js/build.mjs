149658744d13107b0b8a620f0c99f6334ec9e010
Badisi <badisi84@gmail.com>
2022-04-03 17:23:22 +0200

    feat(ci): various improvements

--- projects/auth-js/build.mjs
+++ projects/auth-js/build.mjs
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
-const { buildSync } = require('esbuild');
-const { exec } = require('child_process');
-const { resolve: pathResolve } = require('path');
-const { writeFileSync } = require('fs');
-const { blue, green } = require('chalk');
-const { dependencies, peerDependencies } = require(pathResolve(__dirname, 'package.json'));
+
+import chalk from 'chalk';
+import { exec } from 'child_process';
+import cpy from 'cpy';
+import { buildSync } from 'esbuild';
+import { mkdirSync, readFileSync, writeFileSync } from 'fs';
+import { dirname, resolve as pathResolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const { blue, green } = chalk;
+const { dependencies, peerDependencies } = JSON.parse(readFileSync('package.json'));
 
 const EXTERNALS = Object.keys({ ...dependencies, ...peerDependencies });
 const DIST_PATH = pathResolve(__dirname, '../../dist/auth-js');
@@ -21,8 +27,7 @@ const execCmd = (cmd, opts) => new Promise((resolve, reject) => {
 
 const build = async (entryPointName, platform, distName, bundleExternals = false, minify = false) => {
     const outdir = pathResolve(DIST_PATH, distName, entryPointName);
-
-    buildSync({
+    const options = {
         platform,
         absWorkingDir: __dirname,
         outfile: pathResolve(outdir, (minify) ? 'index.min.js' : 'index.js'),
@@ -33,20 +38,24 @@ const build = async (entryPointName, platform, distName, bundleExternals = false
         minify,
         globalName: (bundleExternals) ? 'AuthJS' : undefined,
         external: (bundleExternals) ? undefined : EXTERNALS
-    });
+    };
 
+    mkdirSync(outdir, { recursive: true });
     if (platform === 'neutral') {
+        options.mainFields = ['module', 'main'];
         writeFileSync(pathResolve(outdir, 'package.json'), JSON.stringify({ type: 'module' }, null, 4), { encoding: 'utf8' });
     } else if (platform === 'node') {
         writeFileSync(pathResolve(outdir, 'package.json'), JSON.stringify({ type: 'commonjs' }, null, 4), { encoding: 'utf8' });
     }
+
+    buildSync(options);
 };
 
 (async () => {
     try {
         console.log(blue('Building Library\n'));
 
-        // Build
+        // Build entry points
         for (const entryPointName of ['core', 'oidc']) { // eslint-disable-line no-loops/no-loops
             console.log('-'.repeat(78));
             console.log(`Building entry point '@badisi/auth-js/${entryPointName}'`);
@@ -67,12 +76,28 @@ const build = async (entryPointName, platform, distName, bundleExternals = false
             console.log(`${green('✓')} Built @badisi/auth-js/${entryPointName}`, '\n');
         }
 
-        // Building types
+        // Build library
         console.log('-'.repeat(78));
-        console.log('Building types for \'@badisi/auth-js\'');
+        console.log('Building \'@badisi/auth-js\'');
         console.log('-'.repeat(78));
+        //  types
         await execCmd('tsc --project tsconfig.lib.json', { cwd: __dirname });
-        console.log(`${green('✓')} Built types\n`);
+        console.log(`${green('✓')} Generating types`);
+        //  assets
+        await cpy('projects/auth-js/oidc/assets', pathResolve(DIST_PATH, 'oidc', 'assets'), { flat: true });
+        await cpy('projects/auth-js/package.json', DIST_PATH, { flat: true });
+        await cpy('projects/auth-js/README.md', DIST_PATH, { flat: true });
+        await cpy('LICENSE', DIST_PATH, { flat: true });
+        console.log(`${green('✓')} Copying assets`);
+        //  package.json
+        const pkgJsonPath = pathResolve(DIST_PATH, 'package.json');
+        const pkgJson = JSON.parse(readFileSync(pkgJsonPath, { encoding: 'utf8' }));
+        delete pkgJson.scripts;
+        delete pkgJson.devDependencies;
+        writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 4), { encoding: 'utf8' });
+        console.log(`${green('✓')} Writing package metadata`);
+        //  end
+        console.log(`${green('✓')} Built @badisi/auth-js\n`);
 
         // Success
         console.log(green('-'.repeat(78)));
