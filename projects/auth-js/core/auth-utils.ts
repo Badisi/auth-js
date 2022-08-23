c6b1b0d68d4f8680deb9adbfeddffb649d84ee3d
Badisi <badisi84@gmail.com>
2022-08-23 15:43:37 +0200

    fix(auth-js): improve AuthUtils.isUrlMatching helper

--- projects/auth-js/core/auth-utils.ts
+++ projects/auth-js/core/auth-utils.ts
@@ -19,7 +19,44 @@ export class AuthUtils {
         }
     };
 
-    public static isUrlMatching = (url1: string, url2?: string): boolean => ((url2 !== undefined) && url1.includes(url2));
+    /**
+     * Matches a full or partial url against another one.
+     * Search parameters will be matched no matter their order.
+     * @param {string} url1 Reference url
+     * @param {string} url2 Url to match against the reference url
+     * @returns {boolean}
+     */
+    public static isUrlMatching = (url1: string, url2?: string): boolean => {
+        if (url2 !== undefined) {
+            const uri1 = new URL(url1, 'http://test');
+            const uri2 = new URL(url2, 'http://test');
+
+            // Check pathnames
+            if ((uri2.pathname !== '') && (uri2.pathname !== '/')) {
+                if (!uri1.pathname.includes(uri2.pathname)) {
+                    return false;
+                }
+            }
+            // Check origins (if uri2 has one)
+            if (uri2.origin !== 'http://test') {
+                if ((uri1.protocol !== uri2.protocol) ||
+                    (uri1.origin !== uri2.origin) ||
+                    !`${uri1.origin}${uri1.pathname}}`.includes(`${uri2.origin}${uri2.pathname}`)
+                ) {
+                    return false;
+                }
+            }
+            // Check search params
+            let matching = true;
+            uri2.searchParams.forEach((value, key) => {
+                if (!uri1.searchParams.getAll(key).includes(value)) {
+                    matching = false;
+                }
+            });
+            return matching;
+        }
+        return false;
+    };
 
     public static getBaseUrl = (): string => {
         const baseUrl = document.baseURI || document.querySelector('base')?.href || location.origin;
