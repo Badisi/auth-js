dc09951cc6f00949cf865ddc9306c4fdbe0e23fb
Badisi <badisi84@gmail.com>
2022-08-23 15:44:17 +0200

    fix(auth-js): jest tests are not working

--- projects/auth-js/jest.config.js
+++ projects/auth-js/jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    displayName: "auth-js",
-    preset: "../../jest.preset.js",
+    displayName: 'auth-js',
+    preset: '../../jest.preset.js',
+    testEnvironment: 'jsdom',
     globals: {
-        "ts-jest": {
-            tsconfig: "<rootDir>/tsconfig.spec.json",
-        },
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json'
+        }
     },
     transform: {
-        "^.+\\.[tj]sx?$": "ts-jest",
+        '^.+\\.[tj]sx?$': 'ts-jest'
     },
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-    coverageDirectory: "../../coverage/projects/auth-js",
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    coverageDirectory: '../../coverage/projects/auth-js'
 };
