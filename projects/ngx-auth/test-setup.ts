da75117a891feefa02c0c68e9e3f7dc32f26b30c
Badisi <badisi84@gmail.com>
2024-04-16 17:07:36 +0200

    fix(jest): error TextEncoder is not defined

--- projects/ngx-auth/test-setup.ts
+++ projects/ngx-auth/test-setup.ts
@@ -1,3 +1,9 @@
+import { TextDecoder, TextEncoder } from 'node:util';
+
+// @ts-expect-error https://mswjs.io/docs/migrations/1.x-to-2.x#requestresponsetextencoder-is-not-defined-jest
+globalThis.TextDecoder = TextDecoder;
+globalThis.TextEncoder = TextEncoder;
+
 // @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
 globalThis.ngJest = {
     testEnvironmentOptions: {
