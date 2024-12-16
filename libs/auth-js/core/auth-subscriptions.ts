1201076c2d120a71ae964b8dea333e5cd118f0f8
Badisi <badisi84@gmail.com>
2024-12-16 14:27:24 +0100

    refactor: use es2022 private properties

--- libs/auth-js/core/auth-subscriptions.ts
+++ libs/auth-js/core/auth-subscriptions.ts
@@ -2,13 +2,13 @@ import type { AuthSubscriber } from './models/auth-subscriber.model';
 import type { AuthSubscription } from './models/auth-subscription.model';
 
 export class AuthSubscriptions<T extends unknown[]> {
-    private subscribers: AuthSubscriber<T>[] = [];
-    private lastNotifiedValue?: T;
+    #subscribers: AuthSubscriber<T>[] = [];
+    #lastNotifiedValue?: T;
 
     public add(subscriber: AuthSubscriber<T>): AuthSubscription {
-        this.subscribers.push(subscriber);
-        if (this.lastNotifiedValue) {
-            void subscriber(...this.lastNotifiedValue);
+        this.#subscribers.push(subscriber);
+        if (this.#lastNotifiedValue) {
+            void subscriber(...this.#lastNotifiedValue);
         }
         return {
             unsubscribe: (): void => { this.unsubscribe(subscriber); }
@@ -16,18 +16,18 @@ export class AuthSubscriptions<T extends unknown[]> {
     }
 
     public notify(...args: T): void {
-        this.lastNotifiedValue = args;
-        this.subscribers.forEach(subscriber => void subscriber(...args));
+        this.#lastNotifiedValue = args;
+        this.#subscribers.forEach(subscriber => void subscriber(...args));
     }
 
     public unsubscribe(subscriber?: AuthSubscriber<T>): void {
         if (subscriber) {
-            const index = this.subscribers.indexOf(subscriber);
+            const index = this.#subscribers.indexOf(subscriber);
             if (index !== -1) {
-                this.subscribers.splice(index, 1);
+                this.#subscribers.splice(index, 1);
             }
         } else {
-            this.subscribers = [];
+            this.#subscribers = [];
         }
     }
 }
