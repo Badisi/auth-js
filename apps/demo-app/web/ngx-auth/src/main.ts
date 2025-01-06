a77c7d76634a79c533edd4a474644deaa3426b71
Badisi <badisi84@gmail.com>
2025-01-06 16:11:14 +0100

    feat(demo-app-ngx-auth): allow to test ng-module as well as standalone

--- apps/demo-app/web/ngx-auth/src/main.ts
+++ apps/demo-app/web/ngx-auth/src/main.ts
@@ -1,11 +1,15 @@
 import { isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { initAuth, provideAuth } from '@badisi/ngx-auth';
 import { DEFAULT_SETTINGS, DemoAppSettings } from 'demo-app-common';
 import pkgJson from 'libs/ngx-auth/package.json';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { AppModule } from './app/app.module';
+
+const USE_STANDALONE = true;
 
 ((): void => {
     window.appSettings = new DemoAppSettings(
@@ -19,12 +23,19 @@ import { appConfig } from './app/app.config';
     document.body.appendChild(el);
 
     initAuth(window.appSettings.getCurrentSettings().librarySettings)
-        .then(authManager => {
+        .then(authProvider => {
             el.replaceWith(document.createElement('app-root'));
 
-            appConfig.providers.push(provideAuth(authManager));
-            bootstrapApplication(AppComponent, appConfig)
-                .catch((err: unknown) => { console.error(err); });
+            // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+            if (USE_STANDALONE) {
+                appConfig.providers.push(provideAuth(authProvider));
+                bootstrapApplication(AppComponent, appConfig)
+                    .catch((err: unknown) => { console.error(err); });
+            } else {
+                platformBrowserDynamic([authProvider])
+                    .bootstrapModule(AppModule)
+                    .catch((err: unknown) => { console.error(err); });
+            }
         })
         .catch((err: unknown) => {
             const message = (err instanceof Error) ? err.message : String(err);
