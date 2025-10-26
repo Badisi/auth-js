47e96a4a07ac796803e13e3b1717334c5b54f7cc
Badisi <Badisi@users.noreply.github.com>
2025-10-26 22:35:16 +0000

    deploy(demo-apps): cb5baf3d45fb0f1e9fa28c0511d688d2e3d065cb

---
+++ demo-app/ngx-auth/chunk-ZXWTRYWV.js
@@ -0,0 +1,1 @@
+import{f as i,g as n}from"./chunk-VSTATLYB.js";import{Y as o,_ as p,da as r,e,ea as t,ga as m}from"./chunk-RBOPQKYT.js";var g,a=e(()=>{p();m();n();g={providers:[o(),r(i,t({onSameUrlNavigation:"reload"}))]}});a();export{g as appConfig};
